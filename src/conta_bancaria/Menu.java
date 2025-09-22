1ad13c652af1371f56c98660ca62ffff53cd8bd7
LeticiaC <leticiafccampos@gmail.com>
2025-09-22 16:42:02 -0300

    :tada: Primeiro commit

---
+++ src/conta_bancaria/Menu.java
@@ -0,0 +1,26 @@
+package conta_bancaria;
+
+import conta_bancaria.model.Conta;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Conta c1 = new Conta(1, 123, 1, "Leticia Campos", 500000.00f);
+			// instanciando objeto classe conta. n conta, agencia conta, tipo conta, titular conta, saldo atual conta, f de float
+		
+			System.out.println("O Saldo da conta é: " + c1.getSaldo());
+			// mostra saldo atual da conta
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
